732f06f035d317e1c5f3846dcf32c270b1879b06
Anas Gamal <agamal@stetson.edu>
2023-11-03 11:10:36 -0400

    Fix: class info scraping not working with some departments

--- stetson-scraper/main.py
+++ stetson-scraper/main.py
@@ -1,5 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
+## The code is still unfinished, it contains debugging code like "failures" and "print" statements
+## The code is also not very robust
+## it will not return correct output for "LAW" department classes. Everything else works.
+
+def parse_class_info(class_string):
+    # Use regular expression to extract department code, class code, class letter, and class name
+    pattern = r"([A-Z]+)\s*(\d+[A-Z]?\d*)([A-Z]?)[\s\.]+(.+?)\.\s+\d"
+    match = re.match(pattern, class_string)
+    
+    if match:
+        department_code = match.group(1)
+        class_code = match.group(2)
+        class_letter = match.group(3)
+        class_name = match.group(4)
+        return [department_code, class_code, class_letter, class_name]
+    else:
+        return None
 
 # URL of the main page with department links
 main_page_url = "https://catalog.stetson.edu/courses-instruction/"
@@ -10,7 +29,7 @@ main_page_content = main_page_response.content
 
 # Parse the main page content
 main_page_soup = BeautifulSoup(main_page_content, "html.parser")
-
+failures = []
 # Extract department links from the main page
 department_links = main_page_soup.select("div.sitemap a.sitemaplink")
 for link in department_links:
@@ -29,8 +48,17 @@ for link in department_links:
         
         class_title = class_block.select_one("p.courseblocktitle strong").text.strip()
         class_description = class_block.select_one("p.courseblockdesc").text.strip()
+        class_info = parse_class_info(class_title)
         
+
         # Print or store the class information as needed
-        print("Class Title:", class_title)
-        print("Class Description:", class_description)
+        if class_info:
+            print("Department Code:", class_info[0])
+            print("Class Code:", class_info[1])
+            print("Class Letter:", class_info[2])
+            print("Class Name:", class_info[3])
+        else:
+            failures.append(class_title)
+
+        print("Failures:", failures)
         print("=" * 50)
