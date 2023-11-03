71a8c76342ef0737c0cdb0f1991c617a2d0dd21b
osterling925 <omsterling76@gmail.com>
2023-11-03 11:24:15 -0400

    classmodel added
    skeleton

---
+++ node/src/models/class.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { toJSON, paginate } = require('./plugins');
+
+const classSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        department: {  // possibly make enum cuz categories?
+            type: String,
+            required: true,
+        },
+        code: {  // course code
+            type: Int16Array,
+            required: true,
+            unique: true,  // cuz each class has its own code
+        },
+        description: {
+            type: String,
+        }
+        keywords: {  // to help in searching for classes
+            type: [String],
+        }
+        isactive: {  // if the course is active or archived/gone
+            type: Boolean,
+            default: true,
+        }
+    }
+);
+
+classSchema.plugin(toJSON);
+classSchema.plugin(paginate);
+
+const Class = mongoose.model('Class', classSchema);
+
+module.exports = Class;
