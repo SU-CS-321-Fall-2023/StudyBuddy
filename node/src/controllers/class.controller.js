1d6c005b9e09ff38473bf3a9d810228091f49dbc
Anas Gamal <agamal@stetson.edu>
2023-11-18 03:31:17 -0500

    Refactor class controller to include flexible search functionality

--- node/src/controllers/class.controller.js
+++ node/src/controllers/class.controller.js
@@ -5,25 +5,33 @@ const catchAsync = require('../utils/catchAsync');
 const { classService } = require('../services');
 
 const getClasses = catchAsync(async (req, res) => {
+  // Get search query
+  const query = req.query.search;
 
-    // Get search query 
-    const query = req.query.search;
-  
-    // Build filter object
-    const filter = {};
-    if(query) {
-      filter.$or = [
-        { department_code: { $regex: query, $options: 'i' } }, 
-        { department_name: { $regex: query, $options: 'i' } },
-        { class_title: { $regex: query, $options: 'i' } }
-      ];
-    }
-  
-    const options = pick(req.query, ['sortBy', 'limit', 'page']);
-  
-    const result = await classService.queryClasses(filter, options);
-    res.send(result);
-  });
+  // Build filter object
+  const filter = {};
+  if (query) {
+    const searchTerms = query.split(' ');
+
+    // Create an array of conditions for each search term
+    const orConditions = searchTerms.map(term => ({
+      $or: [
+        { department_code: { $regex: term, $options: 'i' } },
+        { department_name: { $regex: term, $options: 'i' } },
+        { class_title: { $regex: term, $options: 'i' } },
+        { class_code: { $regex: term, $options: 'i' } },
+      ],
+    }));
+
+    filter.$and = orConditions;
+  }
+
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+
+  const result = await classService.queryClasses(filter, options);
+  res.send(result);
+});
+
 
 
 module.exports = {
