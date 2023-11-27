c51a7340a7ad80224b8b2017c2ff3a60eb86a67d
Rayan Alghamdi <ralghamdi@stetson.edu>
2023-11-27 17:28:51 +0300

    refactor get all classes swagger api documentation

--- node/src/routes/v1/class.route.js
+++ node/src/routes/v1/class.route.js
@@ -29,7 +29,7 @@ module.exports = router;
 
 /**
  * @swagger
- * /classes/Classes:
+ * /classes/all:
  *   get:
  *     summary: Get all classes
  *     description: Retrieve all classes with optional filters.
