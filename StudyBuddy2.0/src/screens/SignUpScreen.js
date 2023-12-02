739eddd2a5c324a7f5decb29449970adc7df4d8d
Sisi Small <smallsianah@gmail.com>
2023-12-02 11:36:39 -0500

    trying to figure out firebase/ google sign in

--- StudyBuddy2.0/src/screens/SignUpScreen.js
+++ StudyBuddy2.0/src/screens/SignUpScreen.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Animated } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+
 
 const SignUpScreen = () => {
   const navigation = useNavigation();
