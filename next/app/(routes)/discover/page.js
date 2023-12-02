0e24fbc38f5bb78542b1f0c08aad92d513e113b5
Anas Gamal <agamal@stetson.edu>
2023-12-02 13:01:23 -0500

    fix key prop missing

--- next/app/(routes)/discover/page.js
+++ next/app/(routes)/discover/page.js
@@ -49,7 +49,7 @@ export default function Page() {
                 <a key={result.id} class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{result.name}</h5>
                 {result.classes.map((cls) => (
-                    <p class="mb-2 text-sm font-medium text-gray-700 dark:text-gray-400">{cls.department_code} {cls.class_code}</p>
+                    <p key={cls.id} class="mb-2 text-sm font-medium text-gray-700 dark:text-gray-400">{cls.department_code} {cls.class_code}</p>
                 ))}
                 </a> 
                 ))}
