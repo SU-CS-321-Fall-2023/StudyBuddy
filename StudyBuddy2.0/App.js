c017697cdff97f1b4d7260fe047cc0353c75450e
Sisi Small <smallsianah@gmail.com>
2023-12-07 17:18:40 -0500

    adds users study group names under profile

--- StudyBuddy2.0/App.js
+++ StudyBuddy2.0/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 // At the top of your app (outside any component)
-if (!global.userStudyGroups) global.userStudyGroups = ['Study Group 1', 'Study Group 2'];
+if (!global.userStudyGroups) global.userStudyGroups = [];
+
 
 
 const App = () => (
