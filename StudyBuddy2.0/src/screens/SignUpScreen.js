5a86a112e2063b3d8ccdbb20842b782cb82cf301
Sisi Small <smallsianah@gmail.com>
2023-12-08 10:43:08 -0500

    Google sign in integrated on sign up screen

--- StudyBuddy2.0/src/screens/SignUpScreen.js
+++ StudyBuddy2.0/src/screens/SignUpScreen.js
@@ -2,12 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Animated } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { ImageBackground } from 'react-native';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 // import auth from '@react-native-firebase/auth';
 // import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-
+WebBrowser.maybeCompleteAuthSession();
 
 const SignUpScreen = () => {
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "508324923582-ncuan2li0la71d0oe8c317lg7l3d8g6d.apps.googleusercontent.com",
+    iosClientId: "508324923582-qtv5h7tf8eub87iett2ek12hu6nflpv2.apps.googleusercontent.com",
+    webClientId: "508324923582-colqt82dke17aa8n8n51msqric08j9sm.apps.googleusercontent.com",
+  });
   const navigation = useNavigation();
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -29,6 +40,48 @@ const SignUpScreen = () => {
 
     return () => clearTimeout(timer); // Clean up the timer if the component unmounts
   }, []);
+
+  useEffect(() => {
+    handleEffect();
+  }, [response, token]);
+
+  async function handleEffect() {
+    const user = await getLocalUser();
+    console.log("user", user);
+    if (!user) {
+      if (response?.type === "success") {
+        // setToken(response.authentication.accessToken);
+        getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(user);
+      console.log("loaded locally");
+    }
+  }
+
+  const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem("@user");
+    if (!data) return null;
+    return JSON.parse(data);
+  };
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
 
   return (
     <ImageBackground
@@ -77,13 +130,43 @@ const SignUpScreen = () => {
   <Text style={styles.buttonText}>Sign Up</Text>
 </TouchableOpacity>
 
-
-<TouchableOpacity
-  style={styles.signUpGoogleButton}
-  onPress={() => navigation.navigate('Home')}
->
-  <Text style={styles.buttonText}>Sign Up with Google</Text>
-</TouchableOpacity>
+<View style={styles.container2}>
+{!userInfo ? (
+        <Button
+          title="Sign in with Google"
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+            navigation.navigate('Home')
+          }}
+          // onPress={async () => {
+          //   await promptAsync();
+          //   if (userInfo) { // Assuming userInfo gets updated with user data on successful login
+          //     // Navigate to the home screen
+          //     navigation.navigate('Home'); // Replace 'Home' with your home screen's name
+          //   }
+          // }}
+        />
+      ) : null 
+      // (
+      //   <View style={styles.card}>
+      //     {userInfo?.picture && (
+      //       <Image source={{ uri: userInfo?.picture }} style={styles.image} />
+      //     )}
+      //     <Text style={styles.text}>Email: {userInfo.email}</Text>
+      //     <Text style={styles.text}>
+      //       Verified: {userInfo.verified_email ? "yes" : "no"}
+      //     </Text>
+      //     <Text style={styles.text}>Name: {userInfo.name}</Text>
+      //     {/* <Text style={styles.text}>{JSON.stringify(userInfo, null, 2)}</Text> */}
+      //   </View>
+      // )
+    }
+    <Button
+        title="remove local store"
+        onPress={async () => await AsyncStorage.removeItem("@user")}
+      />
+</View>
 
       <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>{/* <Button title="Sign Up" onPress={handleSignUp} /> */}
         <Text style={styles.linkText}>Already have an account? 
@@ -154,6 +237,12 @@ const styles = StyleSheet.create({
     width: 150,
     top: 10,
   },
+  container2:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:50,
+  }
 });
 
 export default SignUpScreen;
