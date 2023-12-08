7f1af63565e74c7f516bc08b3b540dc1ec2d7ae2
Sisi Small <smallsianah@gmail.com>
2023-12-08 16:54:46 -0500

    test

--- RNnodeMongo/App.js
+++ RNnodeMongo/App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SignUp from './screens/SignUp';
-import SignIn from './screens/SignIn';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <SignUp />
-  // return <SignIn />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
 
 const styles = StyleSheet.create({
   container: {
