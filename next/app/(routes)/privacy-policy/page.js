4263745b4c74728a4b56fde524daf4806ab8b407
Anas Gamal <agamal@stetson.edu>
2023-12-10 19:48:46 -0500

    add privacy policy page

---
+++ next/app/(routes)/privacy-policy/page.js
@@ -0,0 +1,77 @@
+export default function Page () {
+
+    return (
+      <section className="pt-16 bg-blueGray-50">
+      <div className="w-full lg:w-8/12 px-4 mx-auto">   
+      <div className="flex flex-wrap justify-center pb-3">
+      <div className="max-w-2xl mx-auto mt-8 p-4 bg-white rounded shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Privacy Policy</h2>
+  
+        <div className="mb-4">
+          <p className="font-semibold">Effective Date: 12/08/2023</p>
+        </div>
+  
+        <div className="mb-8">
+          <p>
+            Welcome to StudyBuddy! This Privacy Policy is designed to help you understand how we collect, use, and safeguard your personal information. By using StudyBuddy, you agree to the terms outlined in this Privacy Policy.
+          </p>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">Information We Collect:</h3>
+          <ul className="list-disc pl-6">
+            <li>Account Information: To provide our services, we collect and store information you provide when you register for an account, including your name, email address, and profile picture.</li>
+            <li>Class Information: We gather information about your class or course to connect you with other users in similar classes for a more personalized experience.</li>
+            <li>Chat Data: We collect and store messages exchanged between users to facilitate communication within the app.</li>
+          </ul>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">How We Use Your Information:</h3>
+          <ul className="list-disc pl-6">
+            <li>Connecting Users: We use class information to connect you with other users in the same class, enhancing your app experience.</li>
+            <li>Communication: Your email address is used for account-related communications, and we use chat data to facilitate conversations among users.</li>
+            <li>Improving Services: We may use aggregated and anonymized data to analyze usage patterns and improve our app&apos;s functionality.</li>
+          </ul>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">Sharing of Information:</h3>
+          <p>With Other Users: Your name, profile picture, and class information are visible to other users to facilitate connections and communication.</p>
+          <p>Third-Party Services: We may use third-party services for analytics and support, and your information may be shared with these services to improve our app.</p>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">Data Security:</h3>
+          <p>We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or alteration. However, no method of transmission over the internet or electronic storage is 100% secure, so we cannot guarantee absolute security.</p>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">Your Choices:</h3>
+          <p>You can edit your profile information at any time. You can control who can contact you through app settings.</p>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">Children&apos;s Privacy:</h3>
+          <p>StudyBuddy is intended for users aged 13 and older. We do not knowingly collect personal information from children under 13. If you believe we have collected information from a child under 13, please contact us.</p>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">Updates to Privacy Policy:</h3>
+          <p>We may update this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any material changes through the app or other means.</p>
+        </div>
+  
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-4">Contact Us:</h3>
+          <p>If you have any questions or concerns about our Privacy Policy, please contact us at <a href="mailto:ascarlato@stetson.edu">ascarlato@stetson.edu</a>.</p>
+        </div>
+  
+        <div className="mb-8">
+          <p>By using StudyBuddy, you agree to the terms outlined in this Privacy Policy. StudyBuddy is committed to protecting your privacy and providing a secure and enjoyable experience. Thank you for using StudyBuddy!</p>
+        </div>
+      </div>
+      </div>
+      </div>
+      </section>
+    )
+  }
